a21047de2b920ec3bd29d3da1c7d888f2359fe47
Sulay-Room <sulayman.bah028@gmail.com>
2023-02-13 01:22:03 -0500

    Complete update

--- Client.py
+++ Client.py
@@ -1,7 +1,28 @@
 import socket
+numb = int(input("Please enter a number between 1-100: "))
+name = input("Please enter your full name: ")
+
 
 client = socket.socket()
 client.connect(("localhost",5555))
+#data = client.recv(1024)
+#newdata = data.decode("UTF-8")
+#print(newdata)
+#message = ("Client of "+name+" * "+str(integer)).encode("UTF-8")
+# #client.send((message))
 
-data = client.recv(1024)
-print(data)
+while True:
+    clientname = str("Client of "+name)
+    sendmessage = ((clientname+" "+str(numb)).encode("UTF-8"))
+    client.send(sendmessage)
+    serverm = client.recv(1024)
+    servermn = serverm.decode("UTF-8")
+    before,key,stringname = str(servermn).partition("of ")
+    print(stringname)
+    stringsplit = stringname.split(" ")
+    print(stringsplit)
+    servernumb = stringsplit[2]
+    servern = str(str(stringname).replace(servernumb," ")).rstrip(" ")
+    servername = ("Server of "+servern)
+    sum = numb+int(servernumb)
+    print(clientname+","+servername+","+str(numb)+","+servernumb,"The sum of the numbers is "+str(sum))
--- Server.py
+++ Server.py
@@ -1,13 +1,52 @@
 import socket
+import select
+import threading
+import random
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(("localhost",5555))
-server.listen()
+server.listen(10)
+
+bounce = False
+
+def connectionthread(clientConnection,clientaddress):
+    while True:
+        message = clientConnection.recv(1024)
+        newmessage = message.decode("UTF-8")
+        if newmessage == "exit":
+            break
+        else:
+            print(newmessage)
+            beforekey, halfstring, afterkey = str(newmessage).partition("of ")
+            print(afterkey)
+            splitted = afterkey.split(" ")
+            clientnumb = splitted[2]
+            if int(clientnumb) > 100:
+                print("Number is greater than 100")
+                server.close()
+                break
+            print(clientnumb)
+            name = afterkey.replace(clientnumb,"")
+            print(name)
+            numb = random.randint(1,100)
+            servername = str("Server of "+name+str(numb))
+            sum = int(clientnumb)+numb
+            print(sum)
+            print(servername)
+            clientConnection.send(servername.encode("UTF-8"))
+            break
+
+
+
+
 
 while True:
     (clientConnection,clientaddress) = server.accept()
-    data = b'Hello World'
-
-    clientConnection.send(data)
-
-
+    threading.Thread(target=connectionthread,args=(clientConnection,clientaddress)).start()
+    ##data = 'Hello World'
+    ##newdata = data.encode("UTF-8")
+    ##clientConnection.send(newdata)
+    #message = server.recv(1024)
+    #newmessage = message.decode("UTF-8")
+    #print(newmessage)
